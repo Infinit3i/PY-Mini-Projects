b6b9ec6534e4d74e0cc7ca1ac1a054f143a51e2e
Matthew Iverson <matthewiver@protonmail.com>
2019-10-18 15:12:23 -0500

    add weather.html

--- Applications/weather_app/weather/views.py
+++ Applications/weather_app/weather/views.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+  return render(request, 'weather/weather.html')
