3cf87a120803e5d755c1e83692a0ef49ee0ccb44
Matthew Iverson <matthewiver@protonmail.com>
2019-12-09 12:43:31 -0500

    add multiple enemies

--- Games [PY]/Space Invaders/space_Invaders.py
+++ Games [PY]/Space Invaders/space_Invaders.py
@@ -10,25 +10,24 @@ wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("Space Invaders")
 
-
 border_pen = turtle.Turtle()
 border_pen.speed(0)
 border_pen.color("white")
 border_pen.penup()
 border_pen.setposition(-300,-300)
-border_pen.pensize(3)
 border_pen.pendown()
+border_pen.pensize(3)
 for side in range(4):
-    border_pen.fd(600)
-    border_pen.lt(90)
-    border_pen.hideturtle()
+	border_pen.fd(600)
+	border_pen.lt(90)
+border_pen.hideturtle()	
 
 player = turtle.Turtle()
 player.color("blue")
 player.shape("triangle")
 player.penup()
 player.speed(0)
-player.setposition(0, -250) 
+player.setposition(0, -250)
 player.setheading(90)
 
 playerspeed = 15
@@ -37,103 +36,107 @@ number_of_enemies = 5
 enemies = []
 
 for i in range(number_of_enemies):
-    enemies.append(turtle.Turtle())
+	enemies.append(turtle.Turtle())
 
 for enemy in enemies:
-    enemy = turtle.Turtle()
-    enemy.color("red")
-    enemy.shape("circle")
-    enemy.penup()
-    enemy.speed(0)
-    x = random.randint(-200, 200)
-    y = random.randint(100, 250)
-    enemy.setposition(-200, 250)
+	enemy.color("red")
+	enemy.shape("circle")
+	enemy.penup()
+	enemy.speed(0)
+	x = random.randint(-200, 200)
+	y = random.randint(100, 250)
+	enemy.setposition(x, y)
 
 enemyspeed = 2
 
-laser = turtle.Turtle()
-laser.shapesize(0.5, 0.5)
-laser.setheading(90)
-laser.color("yellow")
-laser.shape("triangle")
-laser.penup()
-laser.speed(0)
-laser.hideturtle()
 
-laserspeed = 20
+bullet = turtle.Turtle()
+bullet.color("yellow")
+bullet.shape("triangle")
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5, 0.5)
+bullet.hideturtle()
 
-laserstate = "ready"
+bulletspeed = 20
 
+
+bulletstate = "ready"
 
 def move_left():
-    x = player.xcor()
-    x -= playerspeed
-    if x < -280:
-        x = 280
-    player.setx(x)
-
+	x = player.xcor()
+	x -= playerspeed
+	if x < -280:
+		x = 280
+	player.setx(x)
+	
 def move_right():
-    x = player.xcor()
-    x += playerspeed
-    if x > 280:
-        x = -280
-    player.setx(x)
-
-def fire_laser():
-    global laserstate
-    if laserstate == "ready":
-        laserstate = "fire"
-        x = player.xcor()
-        y = player.ycor()
-        laser.setposition(x, y + 10)
-        laser.showturtle()
+	x = player.xcor()
+	x += playerspeed
+	if x > 280:
+		x = -280
+	player.setx(x)
+	
+def fire_bullet():
+	global bulletstate
+	if bulletstate == "ready":
+		bulletstate = "fire"
+		x = player.xcor()
+		y = player.ycor() + 10
+		bullet.setposition(x, y)
+		bullet.showturtle()
 
 def isCollision(t1, t2):
-    distance = math.sqrt(math.pow(t1.xcor() - t2.xcor(), 2) + math.pow(t1.ycor() - t2.ycor(),2))
-    if distance < 15:
-        return True
-    else:
-        return False
-
+	distance = math.sqrt(math.pow(t1.xcor()-t2.xcor(),2)+math.pow(t1.ycor()-t2.ycor(),2))
+	if distance < 15:
+		return True
+	else:
+		return False
 turtle.listen()
 turtle.onkeypress(move_left, "Left")
 turtle.onkeypress(move_right, "Right")
-turtle.onkeypress(fire_laser, "space")
+turtle.onkeypress(fire_bullet, "space")
 
 while True:
-    for enemy in enemies:
-        x = enemy.xcor()
-        x += enemyspeed
-        enemy.setx(x)
+	
+	for enemy in enemies:
+		x = enemy.xcor()
+		x += enemyspeed
+		enemy.setx(x)
 
-        if enemy.xcor() > 280:
-            y = enemy.ycor()
-            y -= 20
-            enemyspeed *= -1
-            enemy.sety(y)
-        elif enemy.xcor() < -280:
-            y = enemy.ycor()
-            y -= 20
-            enemyspeed *= -1
-            enemy.sety(y)
+		if enemy.xcor() > 280:
+			y = enemy.ycor()
+			y -= 40
+			enemyspeed *= -1
+			enemy.sety(y)
 
-    if laserstate == "fire":
-        y = laser.ycor()
-        y += laserspeed
-        laser.sety(y)
+		if enemy.xcor() < -280:
+			y = enemy.ycor()
+			y -= 40
+			enemyspeed *= -1
+			enemy.sety(y)
+			
+		if isCollision(bullet, enemy):
+			bullet.hideturtle()
+			bulletstate = "ready"
+			bullet.setposition(0, -400)
+			x = random.randint(-200, 200)
+			y = random.randint(100, 250)
+			enemy.setposition(x, y)
+		
+		if isCollision(player, enemy):
+			player.hideturtle()
+			enemy.hideturtle()
+			print ("Game Over")
+			break
 
-    if laser.ycor() > 275:
-        laser.hideturtle()
-        laserstate = "ready"
+	if bulletstate == "fire":
+		y = bullet.ycor()
+		y += bulletspeed
+		bullet.sety(y)
+	
+    if bullet.ycor() > 275:
+		bullet.hideturtle()
+		bulletstate = "ready"
 
-    if isCollision(laser, enemy):
-        laser.hideturtle()
-        laserstate = "ready"
-        laser.setposition(0, -400)
-        enemy.setposition(-200, 250)
-
-    if isCollision(player, enemy):
-        player.hideturtle()
-        enemy.hideturtle()
-        print("Game Over")
-        break
